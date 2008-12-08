b9b6210ef1b9a04d93bb008368ddad31b713ffdd
mihai.ci <mihai.ci@c2c8f7c8-ae85-11dd-82eb-ff4e721a02f4>
2008-12-08 19:43:59 +0000

    worked on the add new account information screen layout

--- trunk/Aegis Shield/src/com/googlecode/aegisshield/addaccount/AddAccountInformation.java
+++ trunk/Aegis Shield/src/com/googlecode/aegisshield/addaccount/AddAccountInformation.java
@@ -8,6 +8,8 @@ import com.googlecode.aegisshield.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * 	Activity for adding a new user account password information.
@@ -24,6 +26,21 @@ public class AddAccountInformation extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.add_account);
+		
+		Button addAccount = (Button) findViewById(R.id.add_account_button);
+		addAccount.setOnClickListener(new View.OnClickListener() {
+			/**
+			 * 	Add the account information in the the database when the "add account" button
+			 * is pressed.
+			 * 
+			 * @param v
+			 * @see android.view.View.OnClickListener#onClick(android.view.View)
+			 */
+			@Override
+			public void onClick(View v) {
+				//TODO add the stuff in the database.
+			}
+		});
 	}
 	
 }
