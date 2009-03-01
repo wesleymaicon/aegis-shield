d5d4d2d0a67b54f32abfce6171c8edcdd72b05c4
mihai.ci <mihai.ci@c2c8f7c8-ae85-11dd-82eb-ff4e721a02f4>
2009-03-01 18:26:45 +0000

    mishu: added abstractization over the content provider for account information data.

---
+++ Aegis Shield/src/com/googlecode/aegisshield/domain/AccountInformationRepository.java
@@ -0,0 +1,59 @@
+/*
+ * Feb 28, 2009
+ *
+ * This source code is provided under the GPL v3 Open Source license agreement.
+ */
+package com.googlecode.aegisshield.domain;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
+
+/**
+ * 	According to the DDD principles, domain objects should be accessed through repository objects. This 
+ * repository, has the role of abstracting the access to the AccountInformationProvider, as to separate the
+ * activity code from the database access and manipulation code.
+ * 
+ * @author Mihai Campean
+ */
+public class AccountInformationRepository implements DataRepository<AccountInformation> {
+	/**
+	 * 	The content resolver is passed to this repository in order to facilitate database access.
+	 */
+	private ContentResolver resolver;
+	
+	/**
+	 * 	The account information provider for saving and loading account information objects.
+	 */
+	private AccountInformationProvider acctProvider;
+	
+	/**
+	 * Public constructor taking the content resolver as argument.
+	 * 
+	 * @param resolver
+	 */
+	public AccountInformationRepository(ContentResolver resolver) {
+		this.resolver = resolver;
+		acctProvider = new AccountInformationProvider();
+	}
+	
+	/**
+	 * 	Save an AccountInformation object in the application database.
+	 * 
+	 * @param account
+	 * @return
+	 */
+	public Uri save(AccountInformation account) {
+		Uri returnUri = null;
+		ContentValues values = new ContentValues();
+		
+		values.put(AccountInformationProvider.KEY_ACCOUNT_NAME, account.getAccountName());
+		values.put(AccountInformationProvider.KEY_PASSWORD, account.getPassword());  // should be encrypted
+		values.put(AccountInformationProvider.KEY_USER_NAME, account.getUserName());
+		values.put(AccountInformationProvider.KEY_DESCRIPTION, account.getDescription());
+		
+		returnUri = acctProvider.insert(AccountInformationProvider.CONTENT_URI, values);
+		
+		return returnUri;
+	}
+}
