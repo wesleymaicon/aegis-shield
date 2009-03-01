c4941bad2b8e6192e254c7ac4db98d07a8d4ef2c
mihai.ci <mihai.ci@c2c8f7c8-ae85-11dd-82eb-ff4e721a02f4>
2009-03-01 18:26:45 +0000

    mishu: added abstractization over the content provider for account information data.

---
+++ trunk/Aegis Shield/src/com/googlecode/aegisshield/domain/DataRepository.java
@@ -0,0 +1,24 @@
+/*
+ * Mar 1, 2009
+ *
+ * This source code is provided under the GPL v3 Open Source license agreement.
+ */
+package com.googlecode.aegisshield.domain;
+
+import android.net.Uri;
+
+/**
+ * 	Interface for defining the contract of domain repositories. All repositories must implement this
+ * interface. This is also a generic type interface, so it can be used for all types of domain objects
+ * 
+ * @author Mihai Campean
+ */
+public interface DataRepository<E> {
+	/**
+	 * 	The save method for data repositories.
+	 * 
+	 * @param domainObject
+	 * @return
+	 */
+	public Uri save(E domainObject);
+}
