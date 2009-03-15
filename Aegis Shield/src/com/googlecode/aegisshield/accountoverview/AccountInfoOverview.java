400e76280ea11f1ca2b591c7c6398dc5e5ad45b2
mihai.ci <mihai.ci@c2c8f7c8-ae85-11dd-82eb-ff4e721a02f4>
2009-03-15 16:55:04 +0000

    mishu: completed the overview of account information items.

--- Aegis Shield/src/com/googlecode/aegisshield/accountoverview/AccountInfoOverview.java
+++ Aegis Shield/src/com/googlecode/aegisshield/accountoverview/AccountInfoOverview.java
@@ -6,6 +6,13 @@
 package com.googlecode.aegisshield.accountoverview;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.googlecode.aegisshield.R;
+import com.googlecode.aegisshield.domain.AccountInformation;
+import com.googlecode.aegisshield.domain.AccountInformationRepository;
 
 /**
  * 	Activity for displaying all account information in a list.
@@ -13,5 +20,17 @@ import android.app.Activity;
  * @author Mihai Campean
  */
 public class AccountInfoOverview extends Activity {
-//TODO add the view code here...
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.account_overview);
+		
+		ListView itemsView = (ListView) findViewById(R.id.account_list);
+		AccountInformation [] content = new AccountInformationRepository(getContentResolver()).loadAll();
+		ArrayAdapter<AccountInformation> adapter = new ArrayAdapter<AccountInformation>(this, 
+				android.R.layout.simple_list_item_1, content);
+		itemsView.setAdapter(adapter);
+	}
+
 }
