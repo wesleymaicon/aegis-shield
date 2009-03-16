659d8efd8ed842848ab5cb7a155d4b289d4f13f6
mihai.ci <mihai.ci@c2c8f7c8-ae85-11dd-82eb-ff4e721a02f4>
2009-03-16 20:19:07 +0000

    mishu: worked on deleting account info items from the overview list.

--- trunk/Aegis Shield/src/com/googlecode/aegisshield/accountoverview/AccountInfoOverview.java
+++ trunk/Aegis Shield/src/com/googlecode/aegisshield/accountoverview/AccountInfoOverview.java
@@ -7,7 +7,10 @@ package com.googlecode.aegisshield.accountoverview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.googlecode.aegisshield.R;
@@ -26,11 +29,22 @@ public class AccountInfoOverview extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.account_overview);
 		
+		//load the items in the list
 		ListView itemsView = (ListView) findViewById(R.id.account_list);
 		AccountInformation [] content = new AccountInformationRepository(getContentResolver()).loadAll();
 		ArrayAdapter<AccountInformation> adapter = new ArrayAdapter<AccountInformation>(this, 
 				android.R.layout.simple_list_item_1, content);
 		itemsView.setAdapter(adapter);
+		
+		//delete an item from the list
+		Button delete = (Button) findViewById(R.id.delete_account_button);
+		delete.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//TODO add the deletion code here...
+				
+			}
+		});
 	}
 
 }
