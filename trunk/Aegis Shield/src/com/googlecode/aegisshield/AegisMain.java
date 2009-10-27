a102f75012b9850b845efd6a5a0254627d81e0d3
mihai.ci <mihai.ci@c2c8f7c8-ae85-11dd-82eb-ff4e721a02f4>
2009-10-27 13:46:58 +0000

    mishu: added copyright and license information to all source code files.

--- trunk/Aegis Shield/src/com/googlecode/aegisshield/AegisMain.java
+++ trunk/Aegis Shield/src/com/googlecode/aegisshield/AegisMain.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2008 - 2009 Mihai Campean
+ *  
+ *	This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 package com.googlecode.aegisshield;
 
 import android.app.Activity;
@@ -15,6 +31,12 @@ import com.googlecode.aegisshield.app.utils.Constants;
 import com.googlecode.aegisshield.security.auth.AuthenticatorService;
 import com.googlecode.aegisshield.security.crypto.CryptoService;
 
+/**
+ * 	Main activity of the application. Will present the user with a login screen and accept a master password, doing
+ * the validation to see if the password is correct.
+ *
+ * @author Mihai Campean
+ */
 public class AegisMain extends Activity {
 	/**
 	 * 	Application preferences manager.
